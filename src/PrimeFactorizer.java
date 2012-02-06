2292ca56c0b3537c47fecf3a68f11c796d6035eb
unknown <Austin@AustinDesktop.(none)>
2012-02-06 01:18:59 -0600

    Added PrimeFactorizer and moved BigIntPrimeFactorizer over.  Now that I know about longs, I could rewrite all the Primes Tests if I wanted.

--- src/PrimeFactorizer.java
+++ src/PrimeFactorizer.java
@@ -1,33 +1,35 @@
 import java.io.*;
 import java.math.BigInteger;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
  * User: Austin
- * Date: 2/2/12
- * Time: 11:07 PM
+ * Date: 2/5/12
+ * Time: 11:05 PM
  * To change this template use File | Settings | File Templates.
  */
-class PrimeFactorizer {
-
-    private static final String PRIME_RECORD = "C:\\Temp\\PrimeWork\\Primes.txt";
-    private final List<BigInteger> knownPrimes;
-    private final BigInteger BigTwo;
-
+public class PrimeFactorizer {
+    private final List<Integer> knownPrimes;
+    
     public PrimeFactorizer()
     {
-        BigTwo = new BigInteger("2");
-        knownPrimes = new ArrayList<BigInteger>();
-        //System.out.println("Adding Prime Records (This can take time...)");
-        AddPrimesFromRecord();
+        knownPrimes = new ArrayList<Integer>();
     }
 
-    private void AddPrimesFromRecord() {
+    public PrimeFactorizer(String PrimeRecord)
+    {
+        knownPrimes = new ArrayList<Integer>();
+        //System.out.println("Adding Prime Records (This can take time...)");
+        AddPrimesFromRecord(PrimeRecord);
+    }
+
+    private void AddPrimesFromRecord(String primeRecord) {
         FileInputStream fileInputStream = null;
         try {
-            fileInputStream = new FileInputStream(PRIME_RECORD);
+            fileInputStream = new FileInputStream(primeRecord);
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
@@ -37,69 +39,72 @@ class PrimeFactorizer {
         try {
             while((line = reader.readLine()) != null)
             {
-                if(!knownPrimes.contains(new BigInteger(line)))
-                    knownPrimes.add(new BigInteger(line));
+                if(!knownPrimes.contains(Integer.parseInt(line)))
+                    knownPrimes.add(Integer.parseInt(line));
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    boolean IsKnownPrime(BigInteger candidate) {
+    boolean IsKnownPrime(int candidate) {
         return knownPrimes.contains(candidate);
     }
 
-    boolean IsPrime(BigInteger candidate) {
+    boolean IsPrime(int candidate) {
         boolean toReturn = true;
 
-        //System.out.println("Testing prime candidate " + candidate.toString() + " to known primes");
+        //System.out.printf("Testing prime candidate %d to known primes%n", candidate);
         if (IsKnownPrime(candidate)) return true;
         for(int i = 0; i < knownPrimes.size() -1; i++)
         {
-            //System.out.println("Testing prime candidate " + candidate.toString() + " to " + knownPrimes.get(i).toString());
-            if(candidate.remainder(knownPrimes.get(i)).compareTo(BigInteger.ZERO) == 0)
-                toReturn = false;
+            //System.out.printf("Testing prime candidate %d to %s%n", candidate, knownPrimes.get(i).toString());
+            if(candidate % knownPrimes.get(i) == 0)
+                return false;
         }
         if(toReturn)
         {
-            for(BigInteger i = candidate.divide(BigTwo).add(BigInteger.ONE); i.compareTo(knownPrimes.get(knownPrimes.size() - 1)) > 0; i = i.subtract(BigInteger.ONE))
+            int index = 2;
+            if(knownPrimes.size() > 0) index = knownPrimes.get(knownPrimes.size() -1);
+            for(int i= index; i < candidate; i++)
             {
-                //System.out.println("Testing prime candidate " + candidate.toString() + " to " + i.toString());
-                if(candidate.remainder(i).compareTo(BigInteger.ZERO) == 0)
-                    toReturn = false;
+                //System.out.printf("Testing prime candidate %s to %s%n", Integer.toString(candidate), Integer.toString(i));
+                if(candidate % i == 0)
+                    return false;
             }
         }
 
         if(toReturn)
         {
+            //System.out.printf("%s is Prime%n", Integer.toString(candidate));
             knownPrimes.add(candidate);
         }
         return toReturn;
     }
 
-    public BigInteger GetLargestKnownPrimeFactorForValuesUnderMaxKnownPrime(BigInteger maxValue) {
-        if(maxValue.compareTo(knownPrimes.get(knownPrimes.size() -1)) < 0)
+    public int GetLargestKnownPrimeFactorForValuesUnderMaxKnownPrime(int maxValue) {
+        if(maxValue < (knownPrimes.get(knownPrimes.size() -1)))
         {
             for(int knowPrimeIndex = knownPrimes.size() - 1; knowPrimeIndex > 0; knowPrimeIndex--)
             {
-                if(knownPrimes.get(knowPrimeIndex).compareTo(maxValue) <= 0 &&
-                        maxValue.remainder(knownPrimes.get(knowPrimeIndex)).compareTo(BigInteger.ZERO) == 0)
+                if((knownPrimes.get(knowPrimeIndex).compareTo(maxValue) <= 0) &&
+                        ((maxValue % (knownPrimes.get(knowPrimeIndex))) == 0))
                     return knownPrimes.get(knowPrimeIndex);
             }
         }
-        return null;
+        return -1;
     }
-    
-    public void MakePrimesList(BigInteger upTo)
+
+    public void MakePrimesList(int upTo, String primeRecord)
     {
-        for(BigInteger i = knownPrimes.get(knownPrimes.size() -1).add(BigInteger.ONE);i.compareTo(upTo) < 0; i = i.add(BigInteger.ONE))
+        for(int i = 2;i < upTo; i++)
         {
             if(IsPrime(i))
             {
                 try {
-                    FileWriter write = new FileWriter(PRIME_RECORD, true);
+                    FileWriter write = new FileWriter(primeRecord, true);
                     BufferedWriter out = new BufferedWriter(write);
-                    out.write(i.toString() + "\n");
+                    out.write(Integer.toString(i) + "\n");
                     out.close();
                 } catch (IOException e) {
                     e.printStackTrace();
@@ -107,29 +112,23 @@ class PrimeFactorizer {
 
             }
         }
-        
     }
 
-    public BigInteger GetLargetPrimeFactorOverMaxKnowPrime(BigInteger maxValue) {
-        BigInteger toReturn = maxValue;
-        
-        for(int i = 0; i < knownPrimes.size(); i++)
-        {
-            if(toReturn.compareTo(knownPrimes.get(knownPrimes.size() - 1)) < 0)
-                return GetLargestKnownPrimeFactorForValuesUnderMaxKnownPrime(toReturn);
+    public long GetSumPrimesUnderMaxValue(int maxValue) {
+        long toReturn = 0;
 
-            //System.out.println("CurrentValue: " + toReturn.toString() + "     PrimeDivisor: " + knownPrimes.get(i).toString());
-            if(toReturn.remainder(knownPrimes.get(i)).compareTo(BigInteger.ZERO) == 0)
-            {
-                toReturn = toReturn.divide(knownPrimes.get(i));
-                i = -1; //gets reset to 0 once coming around in the for loop
-            }
-        }
-        if(!IsPrime(toReturn))
+        for(int i = 2;i < maxValue; i++)
         {
-            System.out.println("Unable to find the largest Prime Factor.  Got as far as " + toReturn);
-            return null;
+            if(IsPrime(i))
+            {
+                toReturn += i;
+            }
+            if(i % 10000 == 0)
+            {
+                System.out.printf("Sum of Primes:%s\nTesting: %d%n", Long.toString(toReturn), i);
+            }
         }
         return toReturn;
     }
 }
+
