79b904ffb05f77f681d465f61dd779a7099af45f
unknown <Austin@AustinDesktop.(none)>
2012-02-18 13:24:17 -0600

    Added Problem 18

---
+++ src/TrianglePathFinder.java
@@ -0,0 +1,64 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Go IntelliJ IDEA!
+ * User: Austin
+ * Date: 2/12/12
+ * Time: 11:03 AM
+ */
+class TrianglePathFinder {
+    private List<List<Integer>> Triangle;
+    
+    public void Load(String filePath) {
+        FileInputStream fileInputStream = null;
+        try {
+            fileInputStream = new FileInputStream(filePath);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        DataInputStream in = new DataInputStream(fileInputStream);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+        String line;
+        Triangle = new ArrayList<List<Integer>>();
+        try {
+            while((line = reader.readLine()) != null)
+            {
+                List<Integer> width = new ArrayList<Integer>();
+                String[] values = line.split("\\s+");
+                for( String value : values)
+                {
+                    width.add(Integer.parseInt(value));
+                }
+                Triangle.add(width);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public List<List<Integer>> GetTriangle() {
+        return Triangle;
+    }
+
+    public int GetGreatestSumTopToBottom() {
+        //Start with second to last row
+        //Take greater value between (row,column + row + 1, column) and (row, column + row + 1, column + 1)
+        //Move up and do again
+        
+        for(int rowIndex = Triangle.size() - 2; rowIndex >= 0; rowIndex--)
+        {
+            for(int colIndex = 0; colIndex < Triangle.get(rowIndex).size(); colIndex ++)
+            {
+                int leftSum = Triangle.get(rowIndex).get(colIndex) + Triangle.get(rowIndex + 1).get(colIndex);
+                int rightSum = Triangle.get(rowIndex).get(colIndex) + Triangle.get(rowIndex + 1).get(colIndex + 1);
+                if (leftSum < rightSum)
+                    Triangle.get(rowIndex).set(colIndex, rightSum);
+                else
+                    Triangle.get(rowIndex).set(colIndex, leftSum);
+            }
+        }
+        return Triangle.get(0).get(0);
+    }
+}
---
+++ src/TrianglePathFinderTest.java
@@ -0,0 +1,78 @@
+import junit.framework.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Go IntelliJ IDEA!
+ * User: Austin
+ * Date: 2/12/12
+ * Time: 11:03 AM
+ */
+public class TrianglePathFinderTest {
+    @Test
+    public void ShouldLoadTriangleFromFile()
+    {
+        String filePath = "C:\\Temp\\ProjectEuler\\18.SumOfPathsOnTriangleTopToBottom2Lines.txt";
+
+        TrianglePathFinder finder = new TrianglePathFinder();
+        finder.Load(filePath);
+
+        List<List<Integer>> triangle = finder.GetTriangle();
+
+        Assert.assertNotNull(triangle);
+    }
+
+    @Test
+    public void ShouldGetGreatestSumForAllPathsTopToBottom3Lines()
+    {
+        String filePath = "C:\\Temp\\ProjectEuler\\18.SumOfPathsOnTriangleTopToBottom3Lines.txt";
+
+        TrianglePathFinder finder = new TrianglePathFinder();
+        finder.Load(filePath);
+
+        int result = finder.GetGreatestSumTopToBottom();
+
+        Assert.assertEquals(221, result);
+    }
+
+    @Test
+    public void ShouldGetGreatestSumForAllPathsTopToBottom4Lines()
+    {
+        String filePath = "C:\\Temp\\ProjectEuler\\18.SumOfPathsOnTriangleTopToBottom4Lines.txt";
+
+        TrianglePathFinder finder = new TrianglePathFinder();
+        finder.Load(filePath);
+
+        int result = finder.GetGreatestSumTopToBottom();
+
+        Assert.assertEquals(308, result);
+    }
+
+    @Test
+    public void ShouldGetGreatestSumForAllPathsTopToBottom15Lines()
+    {
+        String filePath = "C:\\Temp\\ProjectEuler\\18.SumOfPathsOnTriangleTopToBottom.txt";
+
+        TrianglePathFinder finder = new TrianglePathFinder();
+        finder.Load(filePath);
+
+        int result = finder.GetGreatestSumTopToBottom();
+
+        Assert.assertEquals(1074, result);
+    }
+
+    @Test
+    public void ShouldGetGreatestSumForAllPathsTopToBottom100Lines()
+    {
+        String filePath = "C:\\Temp\\ProjectEuler\\67.triangle.txt";
+
+        TrianglePathFinder finder = new TrianglePathFinder();
+        finder.Load(filePath);
+
+        int result = finder.GetGreatestSumTopToBottom();
+
+        Assert.assertEquals(7273, result);
+    }
+}
