3671dbee8f75eeb598da1511a0476429c757b7e8
Austin <austin183@gmail.com>
2012-02-18 18:25:37 -0600

    Added Problem 21 and 22

---
+++ src/AbundantNumberizer.java
@@ -0,0 +1,57 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Go IntelliJ IDEA!
+ * User: Austin
+ * Date: 2/18/12
+ * Time: 5:17 PM
+ */
+class AbundantNumberizer {
+    private final int Max = 28123;
+    private List<Integer> AbundantNumbers;
+    
+    public boolean IsAbundantNumber(int number) {
+        Factorizer factorizer = new Factorizer();
+        int sumFactors = 0;
+        for (Integer factor : factorizer.GetFactorsOf(number)) {
+            if(factor != number) sumFactors += factor;
+        }
+        return sumFactors > number;
+    }
+
+    public List<Integer> GetAbundantNumbers() {
+        List<Integer> toReturn = new ArrayList<Integer>();
+        
+        for(int i = 1; i <= Max; i++)
+        {
+            if(IsAbundantNumber(i))
+            {
+                toReturn.add(i);
+            }
+        }
+        return toReturn;
+    }
+
+    public int GetSumOfAllNumbersThatCannotBeWrittenAsTheSumOfTwoAbundantNumbers() {
+        int toReturn = 0;
+        for(int numberToTest = 1; numberToTest <= Max; numberToTest++)
+        {
+            if(numberToTest % 1000 == 0) System.out.println("Testing " + numberToTest + " CurrentSum: " + toReturn);
+            if(!CanBeSummedByTwoAbundantNumbers(numberToTest)) toReturn += numberToTest;
+        }
+        return toReturn;
+    }
+    
+    public boolean CanBeSummedByTwoAbundantNumbers(int number) {
+        if(number < 24) return false;
+        if(AbundantNumbers == null)
+            AbundantNumbers = GetAbundantNumbers();
+        
+        for(int i = 0; AbundantNumbers.get(i) < number; i++)
+        {
+            if(AbundantNumbers.contains(number - AbundantNumbers.get(i))) return true;
+        }
+        return false;
+    }
+}
---
+++ src/AbundantNumberizerTest.java
@@ -0,0 +1,76 @@
+import junit.framework.Assert;
+import org.junit.Ignore;
+import org.junit.Test;
+
+import java.util.List;
+
+/**
+ * Go IntelliJ IDEA!
+ * User: Austin
+ * Date: 2/18/12
+ * Time: 5:17 PM
+ */
+public class AbundantNumberizerTest {
+    @Test
+    public void ShouldRecognize1Thru12AsNotAnAbundantNumber()
+    {
+        AbundantNumberizer numberizer = new AbundantNumberizer();
+        for(int number = 1; number < 12; number ++)
+        {
+            boolean result = numberizer.IsAbundantNumber(number);
+
+            Assert.assertEquals(false, result);
+        }
+    }
+
+    @Test
+    public void ShouldRecognize12AsAnAbundantNumber()
+    {
+        AbundantNumberizer numberizer = new AbundantNumberizer();
+        int number = 12;
+        boolean result = numberizer.IsAbundantNumber(number);
+
+        Assert.assertEquals(true, result);
+    }
+
+    @Test
+    public void ShouldFindAllAbundantNumbers()
+    {
+        AbundantNumberizer numberizer = new AbundantNumberizer();
+        List<Integer> abundantNumbers = numberizer.GetAbundantNumbers();
+
+        Assert.assertEquals(6965, abundantNumbers.size());
+    }
+
+    @Test
+    public void ShouldRecognize1CannotBeSummedByTwoAbundantNumbers()
+    {
+        int number = 1;
+
+        AbundantNumberizer numberizer = new AbundantNumberizer();
+        boolean result = numberizer.CanBeSummedByTwoAbundantNumbers(number);
+
+        Assert.assertEquals(false, result);
+    }
+
+    @Test
+    public void ShouldRecognize24CanBeSummedByTwoAbundantNumbers()
+    {
+        int number = 24;
+
+        AbundantNumberizer numberizer = new AbundantNumberizer();
+        boolean result = numberizer.CanBeSummedByTwoAbundantNumbers(number);
+
+        Assert.assertEquals(true, result);
+    }
+
+    @Test
+    @Ignore("Slow")
+    public void ShouldFindSumOfAllNumbersThatCannotBeWrittenAsTheSumOfTwoAbundantNumbers()
+    {
+        AbundantNumberizer numberizer = new AbundantNumberizer();
+        int result = numberizer.GetSumOfAllNumbersThatCannotBeWrittenAsTheSumOfTwoAbundantNumbers();
+
+        Assert.assertEquals(4179871, result);
+    }
+}
