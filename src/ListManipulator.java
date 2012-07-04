e2dd749863822030d6d5edf742abca95a4507460
Austin <austin183@gmail.com>
2012-07-04 14:08:45 -0500

    Commiting to prepare to merge with origin.

--- src/ListManipulator.java
+++ src/ListManipulator.java
@@ -1,3 +1,5 @@
+import java.util.List;
+
 /**
  * Go IntelliJ IDEA!
  * User: Austin
@@ -5,4 +7,22 @@
  * Time: 8:37 AM
  */
 public class ListManipulator {
+
+    public void Swap(List<Integer> toPermute, int i, int k) {
+        int temp = toPermute.get(i);
+        toPermute.set(i, toPermute.get(k));
+        toPermute.set(k, temp);
+    }
+
+    public void Reverse(List<Integer> toPermute, int start)
+    {
+        int end = toPermute.size() - 1;
+        while(start < end)
+        {
+            Swap(toPermute, start, end);
+            start++;
+            end--;
+        }
+
+    }
 }
--- src/SubstitutePrimeFinderTest.java
+++ src/SubstitutePrimeFinderTest.java
@@ -1,3 +1,6 @@
+import org.junit.Assert;
+import org.junit.Test;
+
 /**
  * Go IntelliJ IDEA!
  * User: Austin
@@ -5,4 +8,39 @@
  * Time: 6:15 PM
  */
 public class SubstitutePrimeFinderTest {
+    @Test
+    public void ShouldFindNumberPrimesInSubstitute13()
+    {
+        int expected = 0;
+        int value = 13;
+
+        SubstitutePrimeFinder finder = new SubstitutePrimeFinder();
+        int result = finder.GetHighestNumberPrimesFromSubstitution(value);
+
+        Assert.assertEquals(expected, result);
+    }
+
+    @Test
+    public void ShouldFindNumberPrimesInSubstitute56003()
+    {
+        int expected = 0;
+        int value = 56003;
+
+        SubstitutePrimeFinder finder = new SubstitutePrimeFinder();
+        int result = finder.GetHighestNumberPrimesFromSubstitution(value);
+
+        Assert.assertEquals(expected, result);
+    }
+
+    @Test
+    public void ShouldFindNumberPrimesInSubstitute2()
+    {
+        int expected = 1;
+        int value = 2;
+
+        SubstitutePrimeFinder finder = new SubstitutePrimeFinder();
+        int result = finder.GetHighestNumberPrimesFromSubstitution(value);
+
+        Assert.assertEquals(expected, result);
+    }
 }
